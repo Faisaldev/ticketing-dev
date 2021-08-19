2a12cc284136a307eaf9b5d4fbf5e10503199142
Faisal Ahmed <faisal.dev@gmail.com>
2021-08-19 22:23:40 +0300

    Updates

---
+++ common/src/events/services/base-publisher.ts
@@ -0,0 +1,26 @@
+import { Stan } from 'node-nats-streaming';
+import { Subjects } from '../subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+export abstract class Publisher<T extends Event> {
+  abstract subject: T['subject'];
+
+  constructor(private client: Stan) {}
+
+  publish(data: T['data']): Promise<void> {
+    //publish in NATS is asynchronus operation
+    //so we should return Promise so client can wait if needed so
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), err => {
+        if (err) {
+          return reject(err);
+        }
+        console.log('Event published to subject ', this.subject);
+        resolve();
+      });
+    });
+  }
+}
