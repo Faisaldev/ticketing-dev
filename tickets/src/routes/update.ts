e46fe935f564ead90525f27627dfa0192eb48e3a
Faisal Ahmed <faisal.dev@gmail.com>
2021-08-22 19:52:44 +0300

    publish event on ticket update ticket:updated

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,7 +1,9 @@
 import { NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@faysaltickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { TicketUpdatedPublisher } from '../events/publisher/ticket-updated-publisher';
 import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -27,6 +29,12 @@ router.put('/api/tickets/:id', requireAuth, dataValidation, validateRequest, asy
   });
 
   await ticket.save();
+  await new TicketUpdatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId,
+  });
 
   res.send(ticket);
 });
